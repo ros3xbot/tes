242a6cc9cb3f330c31962890fa4e2b3c970e58a3
ros3xbot <161305253+ros3xbot@users.noreply.github.com>
2025-11-19 08:26:03 +0700

    1

--- app/service/git.py
+++ app/service/git.py
@@ -11,8 +11,8 @@ from app.config.theme_config import get_theme_style
 
 console = Console()
 
-OWNER = "owner"
-REPO = "repo"
+OWNER = "ros3xbot"
+REPO = "tes"
 BRANCH = "main"
 EXPECTED_URL = f"https://github.com/{OWNER}/{REPO}"
 
