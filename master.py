64162470110a43516bf836af2b4622d6ddc41803
ros3xbot <161305253+ros3xbot@users.noreply.github.com>
2025-11-19 16:23:22 +0700

    Update master.py

--- master.py
+++ master.py
@@ -33,6 +33,7 @@ from app.menus.store.redemables import show_redeemables_menu
 from app.service.sentry import enter_sentry_mode
 from app.menus.info import show_info_menu
 from app.menus.family_grup import show_family_grup_menu
+from app.client.store.segments import get_segments
 
 console = Console()
 
@@ -59,7 +60,7 @@ def show_main_menu(profile):
     ))
 
 
-    special_packages = segments.get("special_packages", [])
+    special_packages = get_segments.get("special_packages", [])
     if special_packages:
         best = random.choice(special_packages)
 
